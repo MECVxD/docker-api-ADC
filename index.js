0591977138678dc47b17e7ba81391a3bebd0f7dc
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-01-18 15:47:09 -0600

    Se agregaron el index.js y el .env.example

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const students = requite('./routes/students')
+require('dotenv').config()
+app.use(bodyParser.urlencoded({ extends:false}))
+app.use(bodyParser.json())
+
+app.use('/v1/students', students)
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server start in the port ${process.env.PORT}`)
+})
